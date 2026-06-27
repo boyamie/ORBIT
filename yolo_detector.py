196243e9a3b1b29dfb59dbf31e055a24affbdead
good27189905-lgtm <good27189905@gmail.com>
2026-06-27 11:03:02 +0900

    ⚡️ Add detector

---
+++ yolo_detector.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+class YoloDetector:
+    def __init__(self, model_path="models/yolov8n.pt"):
+        self.model = YOLO(model_path)
+
+    def detect(self, frame):
+        return self.model(frame)
